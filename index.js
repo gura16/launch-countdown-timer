e36a0e1a1bab53be8016841f2dfc9e67d44b19c8
gurami kvakhadze <gkvaxadze@gmail.com>
2023-04-06 21:58:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const countdown = () => {
+  const enddate = new Date("jul 30, 2023 00:00:00").getTime();
+  const now = new Date().getTime();
+
+  const difference = enddate - now;
+
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  const timeDays = Math.floor(difference / days);
+  const timeHours = Math.floor((difference % days) / hours);
+  const timeMinutes = Math.floor((difference % hours) / minutes);
+  const timeSeconds = Math.floor((difference % minutes) / seconds);
+
+  document.getElementById("days").innerHTML = timeDays;
+  document.getElementById("hours").innerHTML = timeHours;
+  document.getElementById("minutes").innerHTML = timeMinutes;
+  document.getElementById("seconds").innerHTML = timeSeconds;
+};
+
+setInterval(countdown, 1000);
